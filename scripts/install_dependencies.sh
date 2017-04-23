4dd7e08e09d564118080381796a09591d0dd0883
jenkins <jenkins@infocloud.com>
2017-04-23 22:44:43 +0800

    modified:   scripts/install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,4 +4,5 @@ iptables -I INPUT -p tcp -m tcp --dport 8080 -j ACCEPT
 service iptables save
 service iptables reload
 
+mdir /var/lib/tomcat6/webapps/HelloSSM
 chown tomcat:tomcat /var/lib/tomcat6/webapps/HelloSSM
