e3e9b171cb238dc22666a77ac865f882a50bc608
jenkins <jenkins@infocloud.com>
2017-04-23 23:08:37 +0800

    modified:   scripts/validate_service.sh 	modified:   src/main/webapp/index.jsp

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-checkResult=`curl -s http://192.168.0.200:8080/HelloSSM/ | grep Welcome  2>&1 > /dev/null; echo $?`
-if [ "$checkResult" == "0" ];then
-    exit 0
-else 
-    exit 1
-fi
+for i in {1..6}
+do
+  checkResult=`curl -s http://192.168.0.200:8080/HelloSSM/ | grep Welcome  2>&1 > /dev/null; echo $?`
+  if [ "$checkResult" == "0" ];then
+      exit 0
+  fi
+  echo "$i try..."
+  sleep 10
+done
+exit 1
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,9 @@
 <%@ page language="java" pageEncoding="UTF-8"%>
 <html>
 <body>
-        Welcome
+    <h1> Welcome </h1>
+    <span>current version: V1.0-b$buildId</span>
+    <button href="/user/userManage/">userList</button>
+
 </body>
 </html>
