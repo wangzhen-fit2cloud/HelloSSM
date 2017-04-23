245b38190d7ac3d23eed2905484a7c62a1d05005
jenkins <jenkins@infocloud.com>
2017-04-23 23:28:05 +0800

    modified:   scripts/validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -2,11 +2,11 @@
 
 for i in {1..6}
 do
-  checkResult=`curl -s http://192.168.0.200:8080/HelloSSM/ | grep Welcome  2>&1 > /dev/null; echo $?`
+  checkResult=`curl -s http://localhost:8080/HelloSSM/ | grep Welcome  2>&1 > /dev/null; echo $?`
   if [ "$checkResult" == "0" ];then
       exit 0
   fi
   echo "$i try..."
-  sleep 10
+  sleep 5
 done
 exit 1
